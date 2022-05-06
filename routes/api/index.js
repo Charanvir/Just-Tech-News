ffac432d60bdb4e496fea95502bba426f5dcb4b8
Charanvir <charanvir123@gmail.com>
2022-05-06 02:44:26 -0400

    updated existing user and post routes to includes comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 
 const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
+const commentRoutes = require("./comments-routes")
 
 // setting api routes for user routes
-router.use('/users', userRoutes)
+router.use('/users', userRoutes);
 // setting api route for post routes
-router.use('/posts', postRoutes)
+router.use('/posts', postRoutes);
+//setting api route for comment route
+router.use('/comments', commentRoutes);
 
 module.exports = router;
