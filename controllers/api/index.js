c870cdfe6421c576f13c830ff3493a658e073d2e
Charanvir <charanvir123@gmail.com>
2022-05-13 23:29:00 -0400

    created login and logout functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,11 @@
-// will serve as a means to sollect all of the API routes and package them up for us
 const router = require('express').Router();
 
-const userRoutes = require("./user-routes");
-const postRoutes = require("./post-routes");
-const commentRoutes = require("./comments-routes")
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-// setting api routes for user routes
 router.use('/users', userRoutes);
-// setting api route for post routes
 router.use('/posts', postRoutes);
-//setting api route for comment route
 router.use('/comments', commentRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-const express = require('express');
-const routes = require("./controllers");
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// middleware function that can take all of the contents of a folder and server them as static assets
-// useful for frontend sepcific files like iamges, stylesheets and JS files
-app.use(express.static(path.join(__dirname, 'public')));
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// turn on routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
-// turn on connection to db and server
-// sync means that it is sequelize taking the models and connecting them to associated database tables
-// by making force: true, we are forcing the sync method to re-create the tables if there are any association changes
-// it is similar to DROP TABLE IF EXISTS
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now Listening!'))
-})
+  app.listen(PORT, () => console.log('Now listening'));
+});
