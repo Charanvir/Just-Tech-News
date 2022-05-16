82495b172523711bf44b1e455a95443f61c439a2
Charanvir <charanvir123@gmail.com>
2022-05-16 01:51:54 -0400

    created dashboard and editing and deleting function of posts

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -101,6 +101,11 @@ const postdata = [
     title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
     post_url: 'https://java.com/at/nibh/in.png',
     user_id: 7
+  },
+  {
+    title: 'Farlo season begins',
+    post_url: 'farlo.com',
+    user_id: 11
   }
 ];
 
--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const exphbs = require('express-handlebars');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: process.env.SESS_Secret,
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -21,7 +21,7 @@ const sess = {
 
 app.use(session(sess));
 
-const helpers = require("./utils/helpers")
+const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
